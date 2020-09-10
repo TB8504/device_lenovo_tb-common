952fa14b017bc90f342229bbd520448ee24902e4
Jarl-Penguin <jarlpenguin@outlook.com>
2020-09-10 12:37:38 +0300

    tb-common: Add color mode/management props
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I6130658ba41c4ff0c7b0dbf6478474768c40ed96

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -49,7 +49,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.enable_hwc_vds=1 \
     setprop vendor.opengles.version=196608 \
     setprop persist.graphics.vulkan.disable=true \
-    ro.vendor.display.cabl=0
+    ro.vendor.display.cabl=0 \
+    ro.sf.hwc_set_default_colormode=true
 
 # DRM
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -83,7 +84,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
-    ro.surface_flinger.max_virtual_display_dimension=4096
+    ro.surface_flinger.max_virtual_display_dimension=4096 \
+    ro.surface_flinger.use_color_management=true
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
