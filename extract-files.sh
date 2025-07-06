52c0f7e911c7488c432bf91886dbc51d774e73ef
adazem009 <info@adazem.com>
2025-07-06 20:52:58 +0000

    tb-common: Patch libwvhidl to load libprotobuf-cpp-lite-v29
    F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.drm@1.2-service.widevine": cannot locate symbol "_ZN6google8protobuf8internal13empty_string_E" referenced by "/system/vendor/lib64/libwvhidl.so"...

--- extract-files.sh
+++ extract-files.sh
@@ -72,4 +72,7 @@ fi
 
 COMMON_BLOB_ROOT="${LINEAGE_ROOT}/vendor/${VENDOR}/${DEVICE_COMMON}/proprietary"
 
+# Fix proprietary blobs
+patchelf --replace-needed "libprotobuf-cpp-lite.so" "libprotobuf-cpp-lite-v29.so" "${COMMON_BLOB_ROOT}/vendor/lib64/libwvhidl.so"
+
 "${MY_DIR}/setup-makefiles.sh"
--- tb.mk
+++ tb.mk
@@ -348,6 +348,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.lineage.build.vendor_security_patch=2018-10-01
 
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite-v29.so
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
