6b76c9fab384b20b748effef3d807d0c5024ae24
adazem009 <info@adazem.com>
2025-07-07 10:26:00 +0000

    tb-common: Patch libsettings to load libprotobuf-cpp-full-v29
    E RILD    : dlopen failed: dlopen failed: cannot locate symbol "_ZN6google8protobuf8internal13empty_string_E" referenced by "/system/vendor/lib64/libsettings.so"...

--- tb.mk
+++ tb.mk
@@ -354,7 +354,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.lineage.build.vendor_security_patch=2018-10-01
 
 PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite-v29.so
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite-v29.so \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full-v29.so
 
 # Vibrator
 PRODUCT_PACKAGES += \
